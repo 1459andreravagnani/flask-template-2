abfe9e3a15fa8a11580f4f70593c91db8a2d3648
1459andreravagnani <andre.ravagnani147@gmail.com>
2020-04-30 14:27:08 -0300

    Atualizção 02

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     pokemons = [
-        ['Bulbasaur', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'],
-        ['Charmander', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png'],
-        ['Squirtle', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png'],
-        ['Pikachu', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png']
+        ['Guaraná  R$: 4.50', 'https://savegnago.vteximg.com.br/arquivos/ids/288962-1000-1000/REFRIGERANTE-ANTARCTICA-350ML-LATA-GUARA.jpg?v=636639069900630000'],
+     
+        ['Pizza R$: 2.50', 'https://img.stpu.com.br/?img=https://s3.amazonaws.com/pu-mgr/default/a0R0f000010xA7GEAU/5c489cd8e4b0842c9b1b6c10.jpg&w=710&h=462'],
+     
+        ['Suco R$: 24.90', 'https://casafiesta.fbitsstatic.net/img/p/suco-de-laranja-integral-prats-900ml-66809/233677.jpg?w=800&h=800&v=no-change'],
+   
+        ['Lanche R$: 18.50', 'https://lh3.googleusercontent.com/proxy/s3xKllKT9sIKGTNuSo1I2FPE_ERAC4N32vN2kh6DA4vIv3Qj2iqGV5ecZQ1bOrQzzgk9UrhNPAbAc3juHtoGC_pK5p_iocavf303ik-IEXW8uVV9rk7_FTK9']
     ]
     return render_template(
         'index.html',
@@ -17,3 +20,5 @@ def index():
  
 if __name__ == '__main__':
     app.run()
+
+    
